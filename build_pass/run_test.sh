5b69dc110945a0dddc8f19594c0f5e87abd49ff9
Sophia M D'Antoine <smdantoine@gmail.com>
2016-07-13 12:13:15 -0400

    updated test script

--- build_pass/run_test.sh
+++ build_pass/run_test.sh
@@ -4,15 +4,16 @@ echo "[+] Compiling tests..."
 ./llvm-3.8.0/build/Release+Asserts/bin/clang -o tests/test1.bc tests/test1.c -c -emit-llvm -Wall -Wshadow -Wextra
 ./llvm-3.8.0/build/Release+Asserts/bin/clang -o tests/test1 tests/test1.c -Wall -Wshadow -Wextra
 
-echo "[+] Running Screen Pass pass..."
+echo "[+] Running screen pass..."
 
 if [[ "${OSTYPE}" == darwin* ]]; then
-	./llvm-3.8.0/build/Release+Asserts/bin/opt -load llvm-3.8.0/build/Release+Asserts/lib/Screen Pass.dylib tests/test1.bc -o tests/test1_transformed.bc -internchallenge
+	./llvm-3.8.0/build/Release+Asserts/bin/opt -load "llvm-3.8.0/build/Release+Asserts/lib/screen.dylib" tests/test1.bc -o tests/test1_transformed.bc -screen
 else
-	./llvm-3.8.0/build/Release+Asserts/bin/opt -load llvm-3.8.0/build/Release+Asserts/lib/Screen Pass.so tests/test1.bc -o tests/test1_transformed.bc -internchallenge
+	./llvm-3.8.0/build/Release+Asserts/bin/opt -load "llvm-3.8.0/build/Release+Asserts/lib/screen.so" tests/test1.bc -o tests/test1_transformed.bc -screen
 fi
-
-echo "[+] Created tests/test1_transformed.bc"
+echo
+echo
+echo "[+] Verifying code with transform: tests/test1_transformed.bc"
 
 echo "[+] Compiling test1_transformed.bc to native executable..."
 ./llvm-3.8.0/build/Release+Asserts/bin/llc tests/test1_transformed.bc -o tests/test1_transformed.o -filetype=obj 
