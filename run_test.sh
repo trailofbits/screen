d2c91da4a878f8fc5ce333f738107d8810fd7190
yan <rottled@gmail.com>
2016-07-19 18:06:15 -0700

    Clean up run_tests.sh

--- run_test.sh
+++ run_test.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
+LLVM_BIN=build/llvm/bin
+case $OSTYPE in
+  darwin*) 
+    EXT=dylib
+    ;;
+  linux*)
+    EXT=so
+    ;;
+  *)
+    echo "Could not detect OS (${OSTYPE})"
+    exit 2
+    ;;
+esac
+
+
+if [ ! -d ${LLVM_BIN} ]; then
+  echo "[!] LLVM not found. Run ./build.sh"
+  exit 1
+fi
+
 echo "[+] Compiling tests..."
-./llvm-3.8.0/build/Release+Asserts/bin/clang -o tests/test1.bc tests/test1.c -c -emit-llvm -Wall -Wshadow -Wextra
-./llvm-3.8.0/build/Release+Asserts/bin/clang -o tests/test1 tests/test1.c -Wall -Wshadow -Wextra
+${LLVM_BIN}/clang -o tests/test1.bc tests/test1.c -c -emit-llvm -Wall -Wshadow -Wextra
+${LLVM_BIN}/clang -o tests/test1 tests/test1.c -Wall -Wshadow -Wextra
 
 echo "[+] Running screen pass..."
-s2n="/home/sophia/SCREEN/s2n/lib/libs2n.bc"
-if [[ "${OSTYPE}" == darwin* ]]; then
-	./llvm-3.8.0/build/Release+Asserts/bin/opt -load "llvm-3.8.0/build/Release+Asserts/lib/screen.dylib" tests/test1.bc -o tests/test1_transformed.bc -screen
-else
-	./llvm-3.8.0/build/Release+Asserts/bin/opt -load "llvm-3.8.0/build/Release+Asserts/lib/screen.so" tests/test1.bc -o tests/test1_transformed.bc -screen
-	echo 
-	#echo 'Running  on s2n'
-	#./llvm-3.8.0/build/Release+Asserts/bin/opt -load "llvm-3.8.0/build/Release+Asserts/lib/screen.so" $s2n -o tests/libs2n_transformed.bc -screen
+${LLVM_BIN}/opt -load build/lib/screen.${EXT} tests/test1.bc -o tests/test1_xformed.bc -screen -screen-output OUTPUT
 
-fi
-echo
 echo
 echo "[+] Verifying paths:"
 echo "B1: main-> printf-> fun1-> anno-> printf-> anno-> printf-> printf"
