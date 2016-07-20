314c13f3b2a547bf399cd6bc1525f5f50377c5c7
yan <rottled@gmail.com>
2016-07-19 20:57:19 -0700

    Make wget's flags more minimal

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ BUILD_DIR=build
 LLVM_DIR=llvm
 FILE=clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 
-echo "Making ${BUILD_DIR}"
+echo "[+] Creating '${BUILD_DIR}'"
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 if [ ! -f ${FILE} ]; then
-  echo "Downloading Clang+LLVM..."
-  wget -q --show-progress http://llvm.org/releases/3.8.1/${FILE}
+  echo "[+] Downloading Clang+LLVM.."
+  wget -q http://llvm.org/releases/3.8.1/${FILE}
 fi
 
 if [ ! -d ${LLVM_DIR} ]; then
-  echo "Unpacking.."
+  echo "[+] Extracting.."
   mkdir ${LLVM_DIR}
   tar xf ${FILE} -C ${LLVM_DIR} --strip-components=1 
 fi
