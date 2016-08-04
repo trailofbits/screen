58d7d9072c35a97943167b652258a2510fb8b1d5
Sophia M D'Antoine <smdantoine@gmail.com>
2016-08-03 21:53:51 -0400

    found bug with example 5 - doesn't find ending annotation even after removing the argc check

--- examples/05_basic_loop.c
+++ examples/05_basic_loop.c
@@ -7,10 +7,10 @@ int main (int argc, char *argv[])
 {
     int amount;
 
-    if (argc < 2) {
+    /*if (argc < 2) {
         fprintf(stderr, "Usage: %s [# of iterations]\n", argv[0]);
         return 1;
-    }
+    }*/
 
     amount = atoi(argv[1]);
 
