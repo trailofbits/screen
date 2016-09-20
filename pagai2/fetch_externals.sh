ed16124c070d0ea83ee1fb5b5b0f0e41aaca2a8b
Sophia D'Antoine <smdantoine@gmail.com>
2016-09-20 12:28:08 -0400

    fetch from s3 bucket

--- pagai2/fetch_externals.sh
+++ pagai2/fetch_externals.sh
@@ -9,23 +9,28 @@ pushd external
 make install-yices
 
 mkdir -p build/apron
-tar xf ~/apron-install.tgz -C build/apron --strip-components=1
+wget https://s3.amazonaws.com/screen-s3/screen-builds/apron-install.tgz
+tar xf ./apron-install.tgz -C build/apron --strip-components=1
 ln -sf build/apron apron
 
 mkdir -p build/boost
-tar xf ~/boost_1_58_0_program_options.tgz -C build/boost --strip-components=1
+wget https://s3.amazonaws.com/screen-s3/screen-builds/boost_1_58_0_program_options.tgz
+tar xf ./boost_1_58_0_program_options.tgz -C build/boost --strip-components=1
 ln -sf build/boost boost
 
 mkdir -p build/llvm
-tar xf ~/clang+llvm-3.8.1_wRTTI.tgz -C build/llvm --strip-components=1
+wget https://s3.amazonaws.com/screen-s3/screen-builds/clang%2Bllvm-3.8.1_wRTTI.tgz
+tar xf ./clang+llvm-3.8.1_wRTTI.tgz -C build/llvm --strip-components=1
 ln -sf build/llvm llvm
 
 mkdir -p build/cudd
-tar xf ~/cudd-2.5.0-build.tgz -C build/cudd --strip-components=1
+wget https://s3.amazonaws.com/screen-s3/screen-builds/cudd-2.5.0-build.tgz
+tar xf ./cudd-2.5.0-build.tgz -C build/cudd --strip-components=1
 ln -sf build/cudd cudd
 
 mkdir -p build/z3
-tar xf ~/z3-inst.tgz -C build/z3 --strip-components=1
+wget https://s3.amazonaws.com/screen-s3/screen-builds/z3-inst.tgz
+tar xf ./z3-inst.tgz -C build/z3 --strip-components=1
 ln -sf build/z3 z3
 
 popd
