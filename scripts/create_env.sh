440ffa1cd623cf6e3dcf426f34ce5605d9f82c87
yan <rottled@gmail.com>
2016-10-05 12:33:13 -0700

    Make create_env.sh more portable

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -15,10 +15,26 @@ if [ "${1}" == "" ]; then
   exit 2
 fi
 
+case $OSTYPE in
+  darwin*) 
+    EXT=dylib
+    ;;
+  linux*)
+    EXT=so
+    ;;
+  *)
+    echo "Could not detect OS (${OSTYPE})"
+    exit 2
+    ;;
+esac
+
+
+echo "export BUILD_DIR=${BUILD}"
 echo "export CC=${BIN}/clang"
 echo "export CXX=${BIN}/clang++"
 echo "export LLVM_LINK=${BIN}/llvm-link"
 echo "export LLVM_OPT=${BIN}/opt"
 echo "export EXTRA_CFLAGS=-emit-llvm"
 echo "export BUILD_BITCODE=1"
-echo "export OPT_FLAGS=\"-load ${BUILD}/lib/screen.so -screen --screen-debug --screen-start-symbol=main --screen-output ${1}\""
+echo "export EXT=${EXT}"
+echo "export OPT_FLAGS=\"-load ${BUILD}/lib/screen.${EXT} -screen --screen-debug --screen-start-symbol=main --screen-output ${1}\""
